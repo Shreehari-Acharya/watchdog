df2cba74d278a221242e6604d4428991112e6793
binit <binitgupta.1711@gmail.com>
2026-03-07 18:56:20 +0530

    feat(installer): remove ZeekTool from installer UI and codebase

--- daemon/cmd/daemon/main.go
+++ daemon/cmd/daemon/main.go
@@ -81,7 +81,6 @@ func RunInstallerUI() {
 		&installers.FalcoTool{},
 		&installers.SuricataTool{},
 		&installers.WazuhTool{},
-		&installers.ZeekTool{},
 	}
 
 	p := tea.NewProgram(ui.InitialModel(tools))
